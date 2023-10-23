6bd22dd979debfcfa825695f7ba6c027f9f82c0e
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-10-23 11:34:50 +0700

    add login functionality in backend side

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,11 +6,6 @@ const createToken = (_id) => {
     return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'})
 }
 
-// login user
-const loginUser = async (req, res) => {
-    return res.json({msg: 'login user'})
-}
-
 // signup user
 const signupUser = async (req, res) => {
     const {email, password} = req.body
@@ -28,6 +23,22 @@ const signupUser = async (req, res) => {
     }
 
 
+}
+
+// login user
+const loginUser = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        const token = createToken(user._id)
+
+        return res.status(200).json({email, token})
+
+    }catch(error){
+        return res.status(400).json({ error: error.message })
+    }
 }
 
 module.exports = {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -50,4 +50,27 @@ userSchema.statics.signup = async function(email, password) {
     return user
 }
 
+// static login method
+userSchema.statics.login = async function(email, password){
+
+    if(!email || !password){
+        throw Error("All fields must be filled")
+    }
+
+    const user = await this.findOne({ email })
+
+    if(!user){
+        throw Error('Incorrect email')
+    }
+
+    // check if password match
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
+
 module.exports = mongoose.model('User', userSchema)
