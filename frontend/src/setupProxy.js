7ee2272a38b13c45d8c8aeb97269a4403f5acb31
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-10-29 00:01:24 +0700

    refactor: setproxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:4000',
+      target: 'https://workout-app-kappa.vercel.app',
       changeOrigin: true,
     })
   );
