36e1a80d3a51051fdaf1c1bdf7bebe4ffba7fef7
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-10-29 10:45:03 +0700

    refactor: proxy file

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: ["http://localhost:3000"],
+        origin: ["https://workout-app-client-eight.vercel.app"],
         methods: ["POST", "GET", "DELETE", "PATCH"],
         credentials: true
     }
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,11 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'https://workout-app-kappa.vercel.app',
-//       changeOrigin: true,
-//     })
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://workout-app-kappa.vercel.app',
+      changeOrigin: true,
+    })
+  );
+};
