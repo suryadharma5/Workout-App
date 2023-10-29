3dce894510db645d5b987302556ff07f2d9ba558
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-10-29 10:17:39 +0700

    refactor: index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: ["https://workout-app-client-eight.vercel.app"],
+        origin: ["http://localhost:3000"],
         methods: ["POST", "GET", "DELETE", "PATCH"],
         credentials: true
     }
